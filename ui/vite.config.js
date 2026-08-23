3729a9479c465fbefe49d45351a7ab64670d4608
lee <ismpink@daum.net>
2026-08-23 14:32:40 +0900

    API연결 수정

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -4,13 +4,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
+  console.log('mode:', mode)
+  console.log('API:', env.VITE_API_URL)
+
   return {
     plugins: [react()],
     server: {
       proxy: {
         '/api': {
-          target: env.VITE_API_BASE_URL,
-          // target: 'http://localhost:3000',
+          target: env.VITE_API_URL,
           changeOrigin: true,
         },
       },
